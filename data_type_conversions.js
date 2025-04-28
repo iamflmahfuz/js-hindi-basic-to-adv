90e7aa61b8f4e2c89bf78a23faa2f7652cb84879
Md Mahfuzur Rahman <mahfuz93623@gmail.com>
2025-04-28 14:48:06 +0600

    data_types_conversions

---
+++ data_type_conversions.js
@@ -0,0 +1,16 @@
+let name = "33"
+
+let nameisnumber = Number(name);
+console.log(nameisnumber);
+console.log(typeof nameisnumber);
+
+let is_not = 0
+let boolean_is_not = Boolean(is_not)
+console.log(is_not)
+console.log(typeof boolean_is_not)
+console.log(boolean_is_not);
+
+// True => 1 ; False =>0
+//"" => False
+//"rahman" => True
+
