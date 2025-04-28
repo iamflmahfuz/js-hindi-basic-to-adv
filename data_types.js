9f9632326e63cdc0cb594b700a1b157375aec4e6
Md Mahfuzur Rahman <mahfuz93623@gmail.com>
2025-04-28 10:30:57 +0600

    js data types

---
+++ data_types.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all codes as newr version
+// number => 2
+// string => "Mahfuz"
+// boolean => true/false
+//undefined=>
+// null => standalone value
+//symmbol => unique
+
+let age=22
+
+
+const name = "mahfuz"
+console.log([typeof age, typeof name]);
+
---
+++ let_const_var-practice-1.js
@@ -0,0 +1,14 @@
+const accountId=102030
+let accountemail="mahfuz93623@gmail.com"
+var password= '018165'
+
+accountcity='Dhaka';
+
+accountemail='iphone93623@gmail.com'
+password='2222222'
+accountcity='Cumilla'
+
+// prefer not to use var because of block scope and functional scope
+
+// accountId=2;
+console.table([accountId,accountemail,password,accountcity]);
